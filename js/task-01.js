ebf8a9429585b096b6a36218023e8329dd24383d
ania.liulka <anialiulka@gmail.com>
2023-04-18 19:54:56 -0400

    Finished task-1

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesEl = document.querySelector("#categories");
+
+const categoriesItemsEl = categoriesEl.children;
+console.log("Number of categories:", categoriesItemsEl.length);
+
+[...categoriesItemsEl].forEach((category) => {
+  const categoryHeading = category.querySelector("h2");
+  console.log("Category: ", categoryHeading.textContent);
+
+  const numberOfIndividualElements = category.querySelectorAll("li").length;
+  console.log("Elements: ", numberOfIndividualElements);
+});
