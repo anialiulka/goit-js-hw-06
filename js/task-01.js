eac7c06b61a770f88497d33181c92531a5c5b613
ania.liulka <anialiulka@gmail.com>
2023-04-20 19:09:22 -0400

    Finished task 2

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,8 @@ const categoriesEl = document.querySelector("#categories");
 const categoriesItemsEl = categoriesEl.children;
 console.log("Number of categories:", categoriesItemsEl.length);
 
+console.log(categoriesItemsEl);
+
 [...categoriesItemsEl].forEach((category) => {
   const categoryHeading = category.querySelector("h2");
   console.log("Category: ", categoryHeading.textContent);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createLiItems = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const listItemEl = document.createElement("li");
+    listItemEl.textContent = ingredient;
+    listItemEl.classList.add("item");
+    return listItemEl;
+  });
+};
+
+const itemLiEl = createLiItems(ingredients);
+
+const ingredientsListEl = document.querySelector("#ingredients");
+ingredientsListEl.append(...itemLiEl);
