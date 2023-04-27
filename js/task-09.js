0f20dde7e80ba9ab328725054b051e96ecc3d1e6
ania.liulka <anialiulka@gmail.com>
2023-04-26 21:33:38 -0400

    Finished task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const colorProperty = document.querySelector(".color");
+
+buttonEl.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorProperty.textContent = bodyEl.style.backgroundColor;
+}
