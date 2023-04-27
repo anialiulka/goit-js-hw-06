add4192dc72acd09a4eab6e341f8e37dc90c741f
ania.liulka <anialiulka@gmail.com>
2023-04-26 21:19:45 -0400

    Finished task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm() {
+  event.preventDefault();
+
+  const elements = {
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+
+  const { email, password } = elements;
+
+  if (email === "" || password === "") {
+    alert("Всі поля мають бути заповнені");
+  }
+
+  const values = { ...elements };
+  console.log(values);
+
+  event.currentTarget.reset();
+}
