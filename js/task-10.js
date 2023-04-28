368b2ffd60964190b67bde87cf13d818440a2086
ania.liulka <anialiulka@gmail.com>
2023-04-27 21:08:31 -0400

    Update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  buttonCreate: document.querySelector("button[data-create]"),
+  buttonDestroy: document.querySelector("button[data-destroy]"),
+  numberPicked: document.querySelector("input[type='number']"),
+  boxes: document.querySelector("#boxes"),
+};
+
+const { buttonCreate, buttonDestroy, numberPicked, boxes } = refs;
+
+buttonCreate.addEventListener("click", onButtonCreate);
+
+buttonDestroy.addEventListener("click", onButtonDestroy);
+
+function onButtonCreate() {
+  createBoxes(numberPicked.value);
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement("div");
+    divEl.style.width = `${30 + 10 * [i]}px`;
+    divEl.style.height = `${30 + 10 * [i]}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    boxes.append(divEl);
+  }
+}
+
+function onButtonDestroy() {
+  boxes.innerHTML = "";
+}
